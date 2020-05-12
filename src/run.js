325fc70232f64aa26dbe2c1e41d4efcffc3e288a
James Shore <jshore@jamesshore.com>
2020-05-12 12:54:28 -0700

    Add command-line runner

--- src/run.js
+++ src/run.js
@@ -1,4 +1,9 @@
 // Copyright Titanium I.T. LLC.
 "use strict";
 
-console.log("TO DO");
+const commandLine = require("./infrastructure/command_line");
+const rot13 = require("./logic/rot13");
+
+const input = commandLine.args()[0];
+const output = rot13.transform(input);
+commandLine.writeOutput(output);
