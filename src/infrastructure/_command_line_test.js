2576aa864e5463d72018ce0855c79423ff10f441
James Shore <jshore@jamesshore.com>
2020-05-15 11:30:14 -0700

    Make 'args' parameter option in CommandLine.runModule

--- src/infrastructure/_command_line_test.js
+++ src/infrastructure/_command_line_test.js
@@ -10,7 +10,7 @@ describe("CommandLine", function() {
 
 	it("provides command-line arguments", async function() {
 		const args = [ "my arg 1", "my arg 2" ];
-		const { stdout } = await runModule("./_command_line_test_args_runner.js", args);
+		const { stdout } = await runModule("./_command_line_test_args_runner.js", { args });
 		assert.equal(stdout, '["my arg 1","my arg 2"]');
 	});
 
@@ -21,14 +21,14 @@ describe("CommandLine", function() {
 	});
 
 	it("writes error output", async function() {
-		const { stdout, stderr } = await runModule("./_command_line_test_output_error_runner.js", [], { failOnError: false });
+		const { stdout, stderr } = await runModule("./_command_line_test_output_error_runner.js", { failOnError: false });
 		assert.equal(stderr, "my error output", "stderr");
 		assert.equal(stdout, "", "stdout");
 	});
 
 });
 
-function runModule(relativeModulePath, args, { failOnError = true } = {}) {
+function runModule(relativeModulePath, { args, failOnError = true } = {}) {
 	return new Promise((resolve, reject) => {
 		const absolutePath = path.resolve(__dirname, relativeModulePath);
 		const options = {
