22f37f7e15213dca0c5ff23c157f910b4f8950df
James Shore <jshore@jamesshore.com>
2020-05-19 12:40:58 -0700

    Factor out common mock setup

--- src/_app_test.js
+++ src/_app_test.js
@@ -9,38 +9,33 @@ const App = require("./app");
 describe("App", function() {
 
 	it("read command-line argument, transform it with ROT-13, and write result", function() {
-		const commandLine = td.object(CommandLine.create());
-		const rot13 = Rot13.create();
-		const app = App.create(commandLine);
-
-		td.when(commandLine.args()).thenReturn([ "my input" ]);
-		const expectedOutput = rot13.transform("my input");
+		const { commandLine, app } = setup(["my input"]);
+		const expectedOutput = Rot13.create().transform("my input");
 
 		app.run();
-
 		td.verify(commandLine.writeOutput(expectedOutput));
 	});
 
 	it("writes usage to command-line when no argument provided", function() {
-		const commandLine = td.object(CommandLine.create());
-		const app = App.create(commandLine);
-
-		td.when(commandLine.args()).thenReturn([]);
-
+		const { commandLine, app } = setup([]);
 		app.run();
-
 		td.verify(commandLine.writeOutput("Usage: run text_to_transform"));
 	});
 
 	it("complains when too many command-line arguments provided", function() {
-		const commandLine = td.object(CommandLine.create());
-		const app = App.create(commandLine);
-
-		td.when(commandLine.args()).thenReturn([ "a", "b" ]);
-
+		const { commandLine, app } = setup([ "a", "b" ]);
 		app.run();
-
 		td.verify(commandLine.writeOutput("too many arguments"));
 	});
 
 });
+
+
+function setup(args) {
+	const commandLine = td.object(CommandLine.create());
+	const app = App.create(commandLine);
+
+	td.when(commandLine.args()).thenReturn(args);
+
+	return { commandLine, app };
+}
