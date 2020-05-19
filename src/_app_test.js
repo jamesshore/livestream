f120fdaec02dc70de870eea505248db81aac2f58
James Shore <jshore@jamesshore.com>
2020-05-19 12:33:50 -0700

    Use spies instead of mocks

--- src/_app_test.js
+++ src/_app_test.js
@@ -1,57 +1,50 @@
 // Copyright Titanium I.T. LLC.
 "use strict";
 
-const sinon = require("sinon");
+const td = require("testdouble");
 const CommandLine = require("./infrastructure/command_line");
 const Rot13 = require("./logic/rot13");
 const App = require("./app");
 
 describe("App", function() {
 
-	afterEach(function() {
-		sinon.reset();
-	});
-
 	it("read command-line argument, transform it with ROT-13, and write result", function() {
-		const commandLineMock = sinon.mock(CommandLine.create());
-		const rot13Mock = sinon.mock(Rot13.create());
-		const app = App.create(commandLineMock.object, rot13Mock.object);
+		const commandLine = td.object(CommandLine.create());
+		const rot13 = td.object(Rot13.create());
+		const app = App.create(commandLine, rot13);
 
-		commandLineMock.expects("args").returns([ "my input" ]);
-		rot13Mock.expects("transform").withExactArgs("my input").returns("my output");
-		commandLineMock.expects("writeOutput").withExactArgs("my output");
+		td.when(commandLine.args()).thenReturn([ "my input" ]);
+		td.when(rot13.transform("my input")).thenReturn("my output");
 
 		app.run();
 
-		commandLineMock.verify();
+		td.verify(commandLine.writeOutput("my output"));
 	});
 
 	it("writes usage to command-line when no argument provided", function() {
-		const commandLineMock = sinon.mock(CommandLine.create());
-		const rot13Mock = sinon.mock(Rot13.create());
-		const app = App.create(commandLineMock.object, rot13Mock.object);
+		const commandLine = td.object(CommandLine.create());
+		const rot13 = td.object(Rot13.create());
+		const app = App.create(commandLine, rot13);
 
-		commandLineMock.expects("args").returns([]);
-		rot13Mock.expects("transform").never();
-		commandLineMock.expects("writeOutput").withExactArgs("Usage: run text_to_transform");
+		td.when(commandLine.args()).thenReturn([]);
+		td.when(rot13.transform("my input")).thenReturn("my output");
 
 		app.run();
 
-		commandLineMock.verify();
+		td.verify(commandLine.writeOutput("Usage: run text_to_transform"));
 	});
 
 	it("complains when too many command-line arguments provided", function() {
-		const commandLineMock = sinon.mock(CommandLine.create());
-		const rot13Mock = sinon.mock(Rot13.create());
-		const app = App.create(commandLineMock.object, rot13Mock.object);
+		const commandLine = td.object(CommandLine.create());
+		const rot13 = td.object(Rot13.create());
+		const app = App.create(commandLine, rot13);
 
-		commandLineMock.expects("args").returns([ "a", "b" ]);
-		rot13Mock.expects("transform").never();
-		commandLineMock.expects("writeOutput").withExactArgs("too many arguments");
+		td.when(commandLine.args()).thenReturn([ "a", "b" ]);
+		td.when(rot13.transform("my input")).thenReturn("my output");
 
 		app.run();
 
-		commandLineMock.verify();
+		td.verify(commandLine.writeOutput("too many arguments"));
 	});
 
 });
