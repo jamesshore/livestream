3625d883db33253d5c975385a15e95be442bd6a2
James Shore <jshore@jamesshore.com>
2020-05-18 22:36:21 -0700

    Make run.js work with classes

--- src/run.js
+++ src/run.js
@@ -1,8 +1,8 @@
 // Copyright Titanium I.T. LLC.
 "use strict";
 
-const commandLine = require("./infrastructure/command_line");
-const rot13 = require("./logic/rot13");
+const commandLine = require("./infrastructure/command_line").create();
+const rot13 = require("./logic/rot13").create();
 
 const input = commandLine.args()[0];
 const output = rot13.transform(input);
