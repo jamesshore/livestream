f11247257279a031dfb6fe2ad3d9ef41a56e0025
James Shore <jshore@jamesshore.com>
2020-06-01 19:05:55 -0700

    Fix Mocha 'uncaughtException' listener leak

--- build/util/mocha_runner.js
+++ build/util/mocha_runner.js
@@ -5,12 +5,23 @@ const Mocha = require("mocha");
 
 exports.runTestsAsync = function(options, success, failure) {
 	return new Promise((resolve, reject) => {
+		// Mocha leaks 'uncaughtException' listeners. So prior to running Mocha, we save the current set of listeners.
+		// Then after running Mocha, we check again and turn off any new ones.
+		const preMochaListeners = process.listeners("uncaughtException");
+
 		const mocha = new Mocha(options.options);
 		options.files.forEach(mocha.addFile.bind(mocha));
-
 		mocha.run(function(failures) {
+			cleanUpListenerLeak(preMochaListeners);
+
 			if (failures) return reject(new Error("Tests failed"));
 			else return resolve();
 		});
 	});
 };
+
+function cleanUpListenerLeak(preMochaListeners) {
+	const postMochaListeners = process.listeners("uncaughtException");
+	const leakedListeners = postMochaListeners.filter((listener) => !preMochaListeners.includes(listener));
+	leakedListeners.forEach(listener => process.off("uncaughtException", listener));
+}
