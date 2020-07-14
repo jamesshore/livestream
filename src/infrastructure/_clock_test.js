04044f5f19c93361d68467be9aa4c6194e7be75c
James Shore <jshore@jamesshore.com>
2020-07-14 12:36:23 -0700

    Create nullable Clock infrastructure wrapper

--- src/infrastructure/_clock_test.js
+++ src/infrastructure/_clock_test.js
@@ -6,5 +6,63 @@ const Clock = require("./clock");
 
 describe("Clock", function() {
 
+	it("provides current timestamp", function() {
+		const clock = Clock.create();
+
+		// Don't inline because that results in a race condition between actual and expected.
+		const actual = clock.now();
+		const expected = Date.now();
+		assert.isAtLeast(actual, expected);
+	});
+
+	it("waits N milliseconds", async function() {
+		const clock = Clock.create();
+
+		const start = clock.now();
+		await clock.waitAsync(10);
+		const elapsedTime = clock.now() - start;
+		assert.isAtLeast(elapsedTime, 10);
+	});
+
+
+	describe("nullability", function() {
+
+		it("defaults 'now' to zero", function() {
+			const clock = Clock.createNull();
+			assert.equal(clock.now(), 0);
+		});
+
+		it("allows 'now' to be configured", function() {
+			const clock = Clock.createNull({ now: 42 });
+			assert.equal(clock.now(), 42);
+		});
+
+		it("can advance the clock", async function() {
+			const clock = Clock.createNull();
+			await clock.advanceNullAsync(10);
+			assert.equal(clock.now(), 10);
+		});
+
+		it("fails fast when attempting to advance the system clock", async function() {
+			const clock = Clock.create();
+			await assert.throwsAsync(
+				() => clock.advanceNullAsync(10),
+				"Can't advance the clock because it isn't a null clock"
+			);
+		});
+
+		it("can wait", async function() {
+			const clock = Clock.createNull();
+			let wait = "waiting";
+
+			clock.waitAsync(10).then(() => {
+				wait = clock.now();
+			});
+			assert.equal(wait, "waiting");
+			await clock.advanceNullAsync(20);
+			assert.equal(wait, 10);
+		});
+
+	});
 
 });
--- src/infrastructure/clock.js
+++ src/infrastructure/clock.js
@@ -1,6 +1,56 @@
 // Copyright Titanium I.T. LLC.
 "use strict";
 
+const FakeTimers = require("@sinonjs/fake-timers");
+
 module.exports = class Clock {
 
+	static create() {
+		return new Clock({
+			Date,
+			setTimeout,
+			advanceNullAsync() { throw new Error("Can't advance the clock because it isn't a null clock"); }
+		});
+	}
+
+	static createNull(options) {
+		return new Clock(nullGlobals(options));
+	}
+
+	constructor(globals) {
+		this._globals = globals;
+	}
+
+	now() {
+		return this._globals.Date.now();
+	}
+
+	async advanceNullAsync(milliseconds) {
+		await this._globals.advanceNullAsync(milliseconds);
+	}
+
+	async waitAsync(milliseconds) {
+		await new Promise((resolve) => {
+			this._globals.setTimeout(() => resolve(), milliseconds);
+		});
+	}
+
 };
+
+
+function nullGlobals({ now = 0 } = {}) {
+	const fake = FakeTimers.createClock(now);
+
+	return {
+		Date: fake.Date,
+
+		async advanceNullAsync(milliseconds) {
+			await fake.tickAsync(milliseconds);
+		},
+
+		setTimeout(fn, milliseconds) {
+			return fake.setTimeout(fn, milliseconds);
+		}
+	};
+
+}
