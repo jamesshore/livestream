54c52359b3a7ca4a334adb6a29c9b9728162d4c9
James Shore <jshore@jamesshore.com>
2020-07-14 12:53:06 -0700

    Add countdown app

--- src/_countdown_test.js
+++ src/_countdown_test.js
@@ -4,10 +4,26 @@
 const assert = require("./util/assert");
 const countdown = require("./countdown");
 const CommandLine = require("./infrastructure/command_line");
+const Clock = require("./infrastructure/clock");
 
 describe("Countdown App", function() {
 
-	it("needs to be implemented", async function() {
+	it("writes one line at a time, waiting one second after each line", async function() {
+		const TEXT = [ "3", "2", "1" ];
+		const commandLine = CommandLine.createNull();
+		const clock = Clock.createNull();
+
+		countdown.countdownAsync(TEXT, commandLine, clock);
+
+		assert.equal(commandLine.getLastStdout(), "3\n");
+		await advanceOneSecondAsync(clock);
+		assert.equal(commandLine.getLastStdout(), "2\n");
+		await advanceOneSecondAsync(clock);
+		assert.equal(commandLine.getLastStdout(), "1\n");
 	});
 
 });
+
+async function advanceOneSecondAsync(clock) {
+	await clock.advanceNullAsync(1000);
+}
--- src/countdown.js
+++ src/countdown.js
@@ -2,7 +2,13 @@
 "use strict";
 
 const CommandLine = require("./infrastructure/command_line");
+const Clock = require("./infrastructure/clock");
 
-exports.countdownAsync = function(text, commandLine = CommandLine.create()) {
-	commandLine.writeStdout("TO DO\n");
+const ONE_SECOND = 1000;
+
+exports.countdownAsync = async function(text, commandLine = CommandLine.create(), clock = Clock.create()) {
+	for (let i = 0; i < text.length; i++) {
+		commandLine.writeStdout(text[i] + "\n");
+		await clock.waitAsync(ONE_SECOND);
+	}
 };
