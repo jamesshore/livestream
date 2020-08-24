1dbb4e0a9639aa15b7aec9e41ba1965884457db0
James Shore <jshore@jamesshore.com>
2020-08-24 12:53:11 -0700

    Stub in HttpClient class

---
+++ src/rot13-cli/infrastructure/_http_client_test.js
@@ -0,0 +1,11 @@
+// Copyright Titanium I.T. LLC.
+"use strict";
+
+const assert = require("util/assert");
+
+describe("HTTP Client", function() {
+
+	it("put tests here", function() {
+	});
+
+});
---
+++ src/rot13-cli/infrastructure/http_client.js
@@ -0,0 +1,10 @@
+// Copyright Titanium I.T. LLC.
+"use strict";
+
+const ensure = require("util/ensure");
+
+/** Generic HTTP client */
+module.exports = class HttpClient {
+
+
+};
