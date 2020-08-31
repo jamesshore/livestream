d3a90536e92dc8ab967a74c4ca3ff76c453c87c2
James Shore <jshore@jamesshore.com>
2020-08-31 15:30:58 -0700

    Stub in Rot13Client and tests

---
+++ src/rot13-cli/infrastructure/_rot13_client_test.js
@@ -0,0 +1,14 @@
+// Copyright Titanium I.T. LLC.
+"use strict";
+
+const assert = require("util/assert");
+const HttpClient = require("./http_client");
+const Rot13Client = require("./rot13_client");
+
+describe("ROT-13 Service client", function() {
+
+	it("does something", function() {
+
+	});
+
+});
---
+++ src/rot13-cli/infrastructure/rot13_client.js
@@ -0,0 +1,10 @@
+// Copyright Titanium I.T. LLC.
+"use strict";
+
+const ensure = require("util/ensure");
+const HttpClient = require("./http_client");
+
+/** ROT-13 service client */
+module.exports = class Rot13Client {
+
+};
