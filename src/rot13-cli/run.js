c0c576b2f737758d56577f29857e88e7ad5bd2c0
James Shore <jshore@jamesshore.com>
2020-09-28 11:53:05 -0700

    Add elapsed time to run.js

--- src/rot13-cli/run.js
+++ src/rot13-cli/run.js
@@ -1,4 +1,12 @@
 // Copyright Titanium I.T. LLC.
 "use strict";
 
-require("./rot13_cli").runAsync();
+runAsync();
+
+async function runAsync() {
+	const start = Date.now();
+	await require("./rot13_cli").runAsync();
+	const elapsedMs = (Date.now() - start);
+	console.log(`(${elapsedMs} ms)`);
+}
+
